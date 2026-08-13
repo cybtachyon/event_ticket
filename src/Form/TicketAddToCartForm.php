8e322eecb4f60ee107be2f14a6fef2987381442f
Derek Reese <derek+github@derekree.se>
2026-08-12 23:08:40 -0700

    Fixes commerce event ticket creation

--- src/Form/TicketAddToCartForm.php
+++ src/Form/TicketAddToCartForm.php
@@ -2,9 +2,15 @@
 
 namespace Drupal\event_ticket\Form;
 
+use Drupal\commerce_cart\CartManagerInterface;
+use Drupal\commerce_cart\CartProviderInterface;
+use Drupal\commerce_order\Resolver\ChainOrderTypeResolverInterface;
+use Drupal\commerce_price\Resolver\ChainPriceResolverInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\commerce_store\SelectStoreTrait;
+use Drupal\Core\Session\AccountInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -17,22 +23,57 @@ class TicketAddToCartForm extends FormBase {
   use SelectStoreTrait;
 
   /**
+   * The Commerce Cart Manager service.
+   *
+   * @var \Drupal\commerce_cart\CartManagerInterface
+   */
+  protected CartManagerInterface $cartManager;
+
+  /**
+   * The Commerce Cart Provider service.
+   *
+   * @var \Drupal\commerce_cart\CartProviderInterface
+   */
+  protected CartProviderInterface $cartProvider;
+
+  /**
+   * The current user Account entity.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected AccountInterface $currentUser;
+
+  /**
    * The entity type manager service.
    *
    * @var \Drupal\Core\Entity\EntityTypeManagerInterface
    */
-  protected $entityTypeManager;
+  protected EntityTypeManagerInterface $entityTypeManager;
+
+  /**
+   * The Commerce Order Type resolver.
+   *
+   * @var \Drupal\commerce_order\Resolver\ChainOrderTypeResolverInterface
+   */
+  protected ChainOrderTypeResolverInterface $orderTypeResolver;
+
+  /**
+   * The Commerce Price resolver.
+   *
+   * @var \Drupal\commerce_price\Resolver\ChainPriceResolverInterface
+   */
+  protected ChainPriceResolverInterface $priceResolver;
 
   /**
    * {@inheritdoc}
    */
-  public static function create(ContainerInterface $container) {
+  public static function create(ContainerInterface $container): self {
     $instance = parent::create($container);
     $instance->cartManager = $container->get('commerce_cart.cart_manager');
     $instance->cartProvider = $container->get('commerce_cart.cart_provider');
     $instance->orderTypeResolver = $container->get('commerce_order.chain_order_type_resolver');
     $instance->currentStore = $container->get('commerce_store.current_store');
-    $instance->chainPriceResolver = $container->get('commerce_price.chain_price_resolver');
+    $instance->priceResolver = $container->get('commerce_price.chain_price_resolver');
     $instance->currentUser = $container->get('current_user');
     $instance->entityTypeManager = $container->get('entity_type.manager');
     return $instance;
@@ -44,7 +85,7 @@ class TicketAddToCartForm extends FormBase {
    * @return string
    *   The unique string identifying the form.
    */
-  public function getFormId() {
+  public function getFormId(): string {
     return 'event_ticket_add_to_cart';
   }
 
@@ -58,6 +99,9 @@ class TicketAddToCartForm extends FormBase {
    *
    * @return array
    *   Form definition array.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $build_info = $form_state->getBuildInfo();
@@ -68,6 +112,7 @@ class TicketAddToCartForm extends FormBase {
     $ticket_storage = $this->entityTypeManager->getStorage('event_ticket');
     $query = $ticket_storage->getQuery();
     $query->condition('event', $event->id());
+    $query->accessCheck(TRUE);
     $ticket_ids = $query->execute();
     $tickets = $ticket_storage->loadMultiple($ticket_ids);
     $form_state->addBuildInfo('event_tickets', $tickets);
@@ -76,14 +121,15 @@ class TicketAddToCartForm extends FormBase {
       '#type' => 'container',
     ];
     foreach ($tickets as $ticket) {
-      $form['tickets'][$ticket->id()] = [
+      $ticket_id = $ticket->id();
+      $form['tickets'][$ticket_id] = [
         '#type' => 'container',
         'summary' => $ticket_view_builder->view($ticket, 'summary'),
         'quantity' => [
           '#type' => 'number',
           '#label' => $this->t('Quantity'),
           '#parents' => [
-            $ticket->id(),
+            $ticket_id,
             'quantity',
           ],
           '#min' => 0,
@@ -106,8 +152,12 @@ class TicketAddToCartForm extends FormBase {
    *   An associative array containing the structure of the form.
    * @param \Drupal\Core\Form\FormStateInterface $form_state
    *   The current state of the form.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Exception
    */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function submitForm(array &$form, FormStateInterface $form_state): void {
     /*
      * At the end of the form have a "Add to cart" button. When clicked, the
      * tickets should be added to the users cart, and they should be sent to
@@ -117,6 +167,7 @@ class TicketAddToCartForm extends FormBase {
     $event = $build_info['event'];
     $tickets = $build_info['event_tickets'];
 
+    /** @var \Drupal\commerce_order\OrderItemStorageInterface $order_item_storage */
     $order_item_storage = $this->entityTypeManager->getStorage('commerce_order_item');
 
     foreach ($tickets as $ticket) {
